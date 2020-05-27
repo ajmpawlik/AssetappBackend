8c41fb0e94abbf79f95121894136e5a36cbbf447
Aleksandra Pawlik <aleksandra.pawlik@alfasystems.com>
2020-05-27 11:50:41 +0100

    changes in persistance config

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,13 +8,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-  @Bean
-  public DataSource dataSource() {
-    DataSourceBuilder builder = DataSourceBuilder.create();
-    builder.url("jdbc:postgresql://localhost:5432/assetapp");
-    builder.username("postgres");
-    builder.password("admin");
-   System.out.println("My custom datasource bean has been intitialised and set");
-    return builder.build();
-  }
+//  @Bean
+//  public DataSource dataSource() {
+//    DataSourceBuilder builder = DataSourceBuilder.create();
+//    builder.url("jdbc:postgresql://localhost:5432/assetapp");
+//    builder.username("postgres");
+//    builder.password("admin");
+//   System.out.println("My custom datasource bean has been intitialised and set");
+//   return builder.build();
+//  }
 }
